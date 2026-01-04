2f63e1b3864fb5cb8c07b4bc5ff77ceb2de85c17
Alex van de Sandt <me@alexvds.com>
2026-01-04 13:46:57 -0600

    Convert to a library and add an error type

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use std::time::Duration;
+
+use futures_util::{SinkExt, StreamExt};
+use tokio_serial::{SerialPortBuilderExt, SerialPortType, SerialStream};
+use tokio_util::codec::{AnyDelimiterCodec, AnyDelimiterCodecError, Framed};
+
+const VENDOR_ID: u16 = 0x1965;
+const PRODUCT_ID: u16 = 0x0017;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("port closed")]
+    PortClosed,
+
+    #[error("scanner not found")]
+    ScannerNotFound,
+
+    #[error(transparent)]
+    Codec(#[from] AnyDelimiterCodecError),
+
+    #[error(transparent)]
+    Serial(#[from] tokio_serial::Error),
+}
+
+#[derive(Debug)]
+pub struct Scanner(Framed<SerialStream, AnyDelimiterCodec>);
+
+impl Scanner {
+    pub fn open() -> Result<Self, Error> {
+        let ports = tokio_serial::available_ports()?;
+        let Some(scanner_port_path) = ports.iter().find_map(|port| {
+            let SerialPortType::UsbPort(usb_port_info) = &port.port_type else {
+                return None;
+            };
+            (usb_port_info.vid == VENDOR_ID && usb_port_info.pid == PRODUCT_ID)
+                .then_some(port.port_name.clone())
+        }) else {
+            return Err(Error::ScannerNotFound);
+        };
+
+        let port = tokio_serial::new(&scanner_port_path, 115200)
+            .timeout(Duration::from_secs(120))
+            .open_native_async()?;
+
+        let framed = Framed::new(port, AnyDelimiterCodec::new(b"\r".to_vec(), b"\r".to_vec()));
+
+        Ok(Self(framed))
+    }
+
+    pub async fn command(&mut self, cmd: &str) -> Result<String, Error> {
+        self.0.send(cmd).await?;
+        let r = self.0.next().await.ok_or(Error::PortClosed)??;
+        Ok(String::from_utf8_lossy(&r).into_owned())
+    }
+
+    pub async fn firmware_version(&mut self) -> Result<String, Error> {
+        self.command("VER").await
+    }
+}
