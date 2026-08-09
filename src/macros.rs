2300a5426ac382a79ea545e3b769a4c83f42d5e3
Alex van de Sandt <me@alexvds.com>
2026-08-08 23:27:33 -0500

    Enable doc comments and fix visibility in macro

--- src/macros.rs
+++ src/macros.rs
@@ -13,29 +13,32 @@ macro_rules! get_set_command {
     // Top level
     (
         text: $text:literal,
+        $(#[$get_doc:meta])?
         get: $get:ident,
+        $(#[$set_doc:meta])?
         set: $set:ident,
         type: $set_name:ident $set_error:tt $set_fields:tt,
     ) => {
-        get_set_command!(get $get $text $set_name);
-        get_set_command!(set $set $text $set_name);
-        get_set_command!(params $set_name $set_error $set_fields);
+        get_set_command!(@get $get $text $set_name $($get_doc)?);
+        get_set_command!(@set $set $text $set_name $($set_doc)?);
+        get_set_command!(@params $set_name $set_error $set_fields);
     };
     // Param set
     (
-        params $set_name:ident ($set_error_name:ident) (
-            $($field_name:ident: $kind:tt $type_name:ident $body:tt),* $(,)?
+        @params $set_name:ident ($set_error_name:ident) (
+            $($(#[$field_doc:meta])? $field_name:ident: $kind:tt $type_name:ident $body:tt),* $(,)?
         )
     ) => {
-        $(get_set_command!(param $kind $type_name $body);)*
+        $(get_set_command!(@param $kind $type_name $body $($field_doc)?);)*
 
         pub struct $set_name {
-            $($field_name: $type_name),*
+            $(pub $field_name: $type_name),*
         }
 
+        #[doc(hidden)]
         impl IntoIterator for $set_name {
             type Item = tokio_util::bytes::Bytes;
-            type IntoIter = std::array::IntoIter<Self::Item, 2>;
+            type IntoIter = std::array::IntoIter<Self::Item, { count_tts!($($field_name)*) }>;
             fn into_iter(self) -> Self::IntoIter {
                 [
                     $(crate::command::IntoParam::into_param(self.$field_name)),*
@@ -85,8 +88,9 @@ macro_rules! get_set_command {
         }
     };
     // Generators for param types
-    (param enum $name:ident { $($variant:ident => $val:literal),* $(,)? }) => {
-        pub enum $name { $($variant),* }
+    (@param enum $name:ident {$($(#[$variant_doc:meta])? $variant:ident => $val:literal),* $(,)? } $($doc:meta)?) => {
+        $(#[$doc])?
+        pub enum $name { $($(#[$variant_doc])? $variant),* }
 
         impl crate::command::IntoParam for $name {
             fn into_param(self) -> tokio_util::bytes::Bytes {
@@ -109,7 +113,8 @@ macro_rules! get_set_command {
         }
     };
     // Commands
-    (get $name:ident $text:literal $response:ident) => {
+    (@get $name:ident $text:literal $response:ident $($doc:meta)?) => {
+        $(#[$doc])?
         pub struct $name;
         impl crate::command::Command for $name {
             const TEXT: &'static [u8] = $text;
@@ -118,8 +123,9 @@ macro_rules! get_set_command {
             fn params(self) -> Self::Params { crate::command::NoParams }
         }
     };
-    (set $name:ident $text:literal $params:ident) => {
-        pub struct $name($params);
+    (@set $name:ident $text:literal $params:ident $($doc:meta)?) => {
+        $(#[$doc])?
+        pub struct $name(pub $params);
         impl crate::command::Command for $name {
             const TEXT: &'static [u8] = $text;
             type Params = $params;
